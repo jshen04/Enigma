ac1a0b0d8d72d9edfa22b6ba36037d6d8d1a9bc2
Jeremy Shen <jshen20@stuy.edu>
2022-05-18 18:47:55 -0400

    changed to Enigma M3 machine, completed?

--- rotor.py
+++ rotor.py
@@ -1,20 +1,21 @@
 import string
 
 class Rotor:
-    def __init__(self, start_key: str, rotor_config: str):
+    def __init__(self, ring_setting: str, notch: str, rotor_config: str):
         self.rotor_wiring = ''.join(rotor_config.split())
-        self.trigger = self.rotor_wiring[0]
+        self.ring_position = string.ascii_uppercase
+        self.trigger = notch
 
-        for _ in range(string.ascii_uppercase.index(start_key.upper())):
-            self.rotate()
-
-        # while self.rotor_wiring[0] != start_key:
-        #     if self.rotor_wiring[0] == start_key:
-        #         break
-        #     self.rotate()
+        for _ in range(string.ascii_uppercase.index(ring_setting.upper())):
+            self.shift()
 
     def rotate(self):
         self.rotor_wiring = self.rotor_wiring[1:] + self.rotor_wiring[0]
+        self.ring_position = self.ring_position[1:] + self.ring_position[0]
+
+    def shift(self):
+        for i, c in enumerate(self.rotor_wiring):
+            self.rotor_wiring =  self.rotor_wiring[0:i] + chr(((ord(c) + 1) - 65 ) % 26 + 65) + self.rotor_wiring[i+1:]
 
     def forward(self, c):
         return self.rotor_wiring[string.ascii_uppercase.index(c.upper())]
