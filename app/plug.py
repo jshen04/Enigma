47f9ae2a76a6603fdf6603b56f5d5c3f48808c3a
Jeremy Shen <jshen20@stuy.edu>
2022-05-22 15:01:56 -0400

    Made more pythonic

--- app/plug.py
+++ app/plug.py
@@ -1,27 +1,25 @@
 from array import array
 import string
 
-class BijectiveMap:
+class Plug:
 
-    def __init__(self, input1:str, input2:str):
+    def __init__(self, input1: str, input2: str):
         self.input1 = input1
         self.input2 = input2
-
-    def link(self,s):
-        alphabet = list(string.ascii_lowercase)
-        newOutput = ""
-        dict = {}
+        self.mapping = {}
         for i in range(26):
-           dict[alphabet[i]] = alphabet[i]
+            self.mapping[string.ascii_uppercase[i]] = string.ascii_uppercase[i]
         for i in range(len(self.input1)):
-            dict[self.input1[i].lower()] = self.input2[i].lower()
-        for i in range(len(self.input1)):   
-            dict[self.input2[i].lower()] = self.input1[i].lower()
-        for k in s:
-            newvalue = dict[k.lower()]
-            print(newvalue)
-            newOutput += newvalue
-        return newOutput
+            self.mapping[self.input1[i].upper()] = self.input2[i].upper()
+        for i in range(len(self.input1)):
+            self.mapping[self.input2[i].upper()] = self.input1[i].upper()
+
+    def link(self, s):
+        ans = ""
+        for c in s:
+            c = self.mapping[c.upper()]
+            ans += c
+        return ans
 
 
 
